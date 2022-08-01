1a373e3ce0f495d4156feeb45231a715e8d63dcc
melashu <meshu102@outlook.com>
2022-08-01 15:32:16 +0300

    Create Js script to display booklist

--- js/books.js
+++ js/books.js
@@ -1,8 +1,44 @@
-let title = document.getElementById('title');
-let author = document.getElementById('author');
-let booklist = document.getElementsByClassName('book-list')[0];
-let addbtn = document.getElementsByClassName('add-btn')[0];
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const booklist = document.getElementsByClassName("book-list")[0];
+const fomr = document.getElementsByClassName("book-form")[0];
 
-let ourbook = [
-
+let allBook = [
+  {
+    title: "Japan",
+    author: "Meshu",
+  },
+  {
+    title: "Japan",
+    author: "DADA",
+  },
 ];
+
+if (localStorage.getItem("ourbook") != null) {
+  allBook = JSON.parse(localStorage.getItem("ourbook"));
+}
+
+let btnRemove;
+
+for (book of allBook) {
+  const bookTemplate = `<div class="book">
+        <p class="book-title">${book["title"]}</p>
+        <p class="book-author">${book["author"]}</p>
+        <button type="button" class="btn-remove">
+    Remove
+  </button>
+    </div>`;
+
+  let domBook = new DOMParser().parseFromString(bookTemplate, "text/html");
+  const bookContainer = domBook.querySelector(".book");
+  booklist.appendChild(bookContainer);
+}
+
+btnRemove = document.querySelectorAll(".book .btn-remove");
+
+btnRemove.forEach((remove) => {
+  remove.addEventListener("click", (e) => {
+    let parent = e.target.parentNode;
+    booklist.removeChild(parent);
+  });
+});
