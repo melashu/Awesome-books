621a5635e1a64794ce37700e0ae22759eb0e6de6
Aseni <alusinejalloh800@gmail.com>
2022-08-01 11:57:04 +0300

    add js variables and array object in the js

---
+++ js/books.js
@@ -0,0 +1,8 @@
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let booklist = document.getElementsByClassName('book-list')[0];
+let addbtn = document.getElementsByClassName('add-btn')[0];
+
+let ourbook = [
+
+];
